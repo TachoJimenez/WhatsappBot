a6880021f81e75b304633ab794b011facccbdc9a
Tacho Jimenez <noeljim181@gmail.com>
2026-02-13 02:44:33 -0600

    Se a echo cambios en la conexion para mas seguridad

--- conexion.js
+++ conexion.js
@@ -1,22 +1,23 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 class Conexion {
-  constructor() {
-    this.pool = mysql.createPool({
-      host: '127.0.0.1',
-      user: 'bot',
-      password: 'BotClave123!',
-      database: 'whatsapp_bot',
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
-  }
+    constructor() {
+        this.pool = mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0
+        });
+    }
 
-  async query(sql, params = []) {
-    const [rows] = await this.pool.execute(sql, params);
-    return rows;
-  }
+    async query(sql, params = []) {
+        const [rows] = await this.pool.execute(sql, params);
+        return rows;
+    }
 }
 
 module.exports = new Conexion();
